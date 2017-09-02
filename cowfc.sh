13ebbaa5750bea67dd0839a2a3377a5891a78ba2
kyle95wm <kyle95wm@gmail.com>
2017-09-02 17:03:29 -0400

    Create placeholder for installer script

---
+++ cowfc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# DWC Network Installer script by kyle95wm/beanjr - re-written for CoWFC
+
+# We need to add stuff here. We could take ideas from dwc_network_installer script
+echo "Nothing to see here!"
+exit
+
