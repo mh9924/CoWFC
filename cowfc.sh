9fe3e9fd09c830c7475cd8fa90fe26b41cb02169
kyle95wm <kyle95wm@gmail.com>
2017-09-09 16:21:15 -0400

    More progress

--- cowfc.sh
+++ cowfc.sh
@@ -12,10 +12,10 @@ vh1="gamestats2.gs.nintendowifi.net.conf" # This is the first virtual host file
 vh2="gamestats.gs.nintendowifi.net.conf" # This is the second virtual host file
 vh3="nas-naswii-dls1-conntest.nintendowifi.net.conf" # This is the third virtual host file
 vh4="sake.gs.nintendowifi.net.conf" # This is the fourth virtual host file
-vh9="gamestats2.gs.nintendowifi.net.conf"
-vh10="gamestats.gs.nintendowifi.net.conf"
-vh11="nas-naswii-dls1-conntest.nintendowifi.net.conf"
-vh12="sake.gs.nintendowifi.net.conf"
+vh5="gamestats2.gs.nintendowifi.net.conf"
+vh6="gamestats.gs.nintendowifi.net.conf"
+vh7="nas-naswii-dls1-conntest.nintendowifi.net.conf"
+vh8="sake.gs.nintendowifi.net.conf"
 mod1="proxy" # This is a proxy mod that is dependent on the other 2
 mod2="proxy_http" # This is related to mod1
 fqdn="localhost" # This variable fixes the fqdn error in Apache
@@ -86,6 +86,70 @@ a2ensite *.nintendowifi.net.conf
 service apache2 restart
 }
 
+function create_apache_vh_wiimmfi {
+# This function will create virtual hosts for Wiimmfi's domains in Apache
+echo "Creating Wiimmfi virtual hosts...."
+touch /etc/apache2/sites-available/gamestats2.gs.wiimmfi.de.conf
+touch /etc/apache2/sites-available/gamestats.gs.wiimmfi.de.conf
+touch /etc/apache2/sites-available/nas-naswii-dls1-conntest.wiimmfi.de.conf
+touch /etc/apache2/sites-available/sake.gs.wiimmfi.de.conf
+cat >/etc/apache2/sites-available/gamestats2.gs.wiimmfi.de.conf <<EOF
+<VirtualHost *:80>
+ServerAdmin webmaster@localhost
+ServerName gamestats2.gs.wiimmfi.de
+ServerAlias "gamestats2.gs.wiimmfi.de, gamestats2.gs.wiimmfi.de"
+ProxyPreserveHost On
+ProxyPass / http://127.0.0.1:9002/
+ProxyPassReverse / http://127.0.0.1:9002/
+</VirtualHost>
+EOF
+
+cat >/etc/apache2/sites-available/gamestats.gs.wiimmfi.de.conf <<EOF
+<VirtualHost *:80>
+ServerAdmin webmaster@localhost
+ServerName gamestats.gs.wiimmfi.de
+ServerAlias "gamestats.gs.wiimmfi.de, gamestats.gs.wiimmfi.de"
+ProxyPreserveHost On
+ProxyPass / http://127.0.0.1:9002/
+ProxyPassReverse / http://127.0.0.1:9002/
+</VirtualHost>
+EOF
+
+cat >/etc/apache2/sites-available/nas-naswii-dls1-conntest.wiimmfi.de.conf <<EOF
+<VirtualHost *:80>
+ServerAdmin webmaster@localhost
+ServerName naswii.wiimmfi.de
+ServerAlias "naswii.wiimmfi.de, naswii.wiimmfi.de"
+ServerAlias "nas.wiimmfi.de"
+ServerAlias "nas.wiimmfi.de, nas.wiimmfi.de"
+ServerAlias "dls1.wiimmfi.de"
+ServerAlias "dls1.wiimmfi.de, dls1.wiimmfi.de"
+ServerAlias "conntest.wiimmfi.de"
+ServerAlias "conntest.wiimmfi.de, conntest.wiimmfi.de"
+ProxyPreserveHost On
+ProxyPass / http://127.0.0.1:9000/
+ProxyPassReverse / http://127.0.0.1:9000/
+</VirtualHost>
+EOF
+
+cat >/etc/apache2/sites-available/sake.gs.wiimmfi.de.conf <<EOF
+<VirtualHost *:80>
+ServerAdmin webmaster@localhost
+ServerName sake.gs.wiimmfi.de
+ServerAlias sake.gs.wiimmfi.de *.sake.gs.wiimmfi.de
+ServerAlias secure.sake.gs.wiimmfi.de
+ServerAlias secure.sake.gs.wiimmfi.de *.secure.sake.gs.wiimmfi.de
+ProxyPass / http://127.0.0.1:8000/
+CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+echo "Done!"
+echo "enabling...."
+a2ensite *.wiimmfi.de.conf
+service apache2 restart
+}
+
 function dns_config {
 # This function will configure dnsmasq
 echo "----------Lets configure DNSMASQ now----------"
@@ -108,6 +172,7 @@ echo "Please type in either your LAN or external IP"
 read -e IP
 cat >>/etc/dnsmasq.conf <<EOF # Adds your IP you provide to the end of the DNSMASQ config file
 address=/nintendowifi.net/$IP
+address=/wiimmfi.de/$IP
 EOF
 clear
 echo "DNSMasq setup completed!"
@@ -117,8 +182,29 @@ clear
 }
 
 functiion install_required_packages {
-# A function to install the required packages. Please feel free to add 
-# instructions here for package installs. I won't add anything here yet since the server is being re-written in C++
+# Add PHP 7.1 repo
+echo "Adding the PHP 7.1 repository. Please follow any prompts."
+add-apt-repository ppa:ondrej/php
+sleep 2s
+echo "Updating & installing PHP 7.1 onto your system..."
+apt-get update
+apt-get install php7.1 -y
+# Now installing the other packages
+}
+function config_mysql {
+echo "We will now configure MYSQL server."
+read -p "Please enter the MYSQL password you would like to use for user 'root': " MYSQLPASSWD
+echo "Great! I will now install and configure MYSQL Server with the password you gave me."
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQLPASSWD'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQLPASSWD'
+apt-get -y install mysql-server
+# We will now set the new mysql password in the AdminPage.php file.
+# Do not change "passwordhere", as this will be the base for replacing it later
+# The below sed command has NOT been tested so we don't know if this will work or not.
+sed -i -e 's/passwordhere/$MYSQLPASSWD/g' /var/www/html/_site/AdminPage.php
+# Next we will install two more packages to make mysql and sqlite work with PHP
+apt-get install php7.1-mysql -y
+apt-get install sqlite php-sqlite3 -y
 }
 
 function check_curl {
@@ -129,4 +215,22 @@ if [ $? != 0 ] ; then
 fi
 }
 
-### Feel free to add more stuff.
+# MAIN
+# First we will check if we are on Ubuntu
+if [  -n "$(uname -a | grep Ubuntu)" ]; then
+    CANRUN="TRUE"
+else
+    CANRUN="FALSE"
+fi
+
+# Determine if our script can run
+if [ $CANRUN == "TRUE" ] ; then
+# Our script can run since we are on Ubuntu
+# Put commands or functions on these lines to continue with script execution.
+# The first thing we will do is to update our package repos
+apt-get update
+else
+    echo "Sorry, you do not appear to be running a supported Opperating System."
+    echo "Please make sure you are running the latest version of Ubuntu and try again!"
+    exit 1
+fi
