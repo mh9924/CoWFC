1eca28f22e726f7ad765e4639d20f5567fbbcfcd
kyle95wm <kyle95wm@gmail.com>
2017-12-03 20:18:06 -0500

    Braces will forever be the death of me

--- Web/_admin/User List.php
+++ Web/_admin/User List.php
@@ -86,6 +86,7 @@ final class UserList extends AdminPage {
 					echo "<input type='hidden' name='action' id='action' value='profileunban'><input type='hidden' name='identifier' id='identifier' value='{$nasdata['gsbrcd']}'><input type='submit' class='btn btn-primary' value='Unban Profile'>";
 				} else {
 					echo "<input type='hidden' name='action' id='action' value='profileban'><input type='hidden' name='identifier' id='identifier' value='{$nasdata['gsbrcd']}'><input type='text' class='form-control' placeholder='Reason' name='reason' id='reason' style='width: 100px;'><input type='text' class='form-control' placeholder='# minutes' name='time' id='time' style='width: 100px;' value='0' maxlength='11'><input type='submit' class='btn btn-primary' value='Ban Profile'>";
+				}
 			} else {
 				echo "<td>N/A</td>";
 			echo "<td>{$row[3]}</td>";
