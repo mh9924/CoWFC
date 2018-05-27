620e8364e824b0c6b261e87ec65f1bdc67291dca
kyle95wm <kyle95wm@gmail.com>
2018-05-27 14:59:51 -0400

    No longer prompt for rank number. Default is 3

--- add-acct.sh
+++ add-acct.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read -p "Please enter a username: " username
-read -p "Please enter a password, or leave blank if you only have a hash: " password
-read -p "Please enter a rank number [1-3]: " rank
+read -rp "Please enter a username: " username
+read -rp "Please enter a password, or leave blank if you only have a hash: " password
+#read -rp "Please enter a rank number [1-3]: " rank
 if [ ! -z $password ] ; then
 	hash=$(/var/www/CoWFC/SQL/bcrypt-hash "$password")
 else
@@ -10,4 +10,5 @@ else
 fi
 
 echo "Inserting user $username into db....."
-echo "insert into users (Username, Password, Rank) values ('$username','$hash','$rank');" | mysql -u root -ppasswordhere cowfc
+#echo "insert into users (Username, Password, Rank) values ('$username','$hash','$rank');" | mysql -u root -ppasswordhere cowfc
+echo "insert into users (Username, Password, Rank) values ('$username','$hash','3');" | mysql -u root -ppasswordhere cowfc
